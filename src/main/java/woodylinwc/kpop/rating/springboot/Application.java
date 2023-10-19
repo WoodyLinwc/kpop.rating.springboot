4fea1ca8ffbaa1e08c6eb1c6afadec043c342a99
Woody Lin <wancheng.lin001@umb.edu>
2023-10-19 11:41:59 -0400

    Initial commit

---
+++ src/main/java/woodylinwc/kpop/rating/springboot/Application.java
@@ -0,0 +1,17 @@
+package woodylinwc.kpop.rating.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	// @GetMapping("/")
+	// public String apiRoot(){
+	// 	return "Hello World";
+	// }
+}
