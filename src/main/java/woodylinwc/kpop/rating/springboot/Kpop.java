8f71b6a45908443956a0f73b982d637f89efb1ac
Woody Lin <wancheng.lin001@umb.edu>
2023-10-28 22:14:08 -0400

    Update Kpop.java

--- src/main/java/woodylinwc/kpop/rating/springboot/Kpop.java
+++ src/main/java/woodylinwc/kpop/rating/springboot/Kpop.java
@@ -3,17 +3,31 @@ package woodylinwc.kpop.rating.springboot;
 import java.util.List;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.DocumentReference;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Document(collection = "kpop")
+// getter and setter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Kpop {
 
+    @Id
     private ObjectId id;
-    private String imdbID;
+    private String imdbId;
     private String title;
     private String releaseDate;
     private String trailerLink;
     private String poster;
     private List<String> genres;
+    private List<String> backdrops;
+    @DocumentReference
+    private List<Review> reviewIds;
     
 }
