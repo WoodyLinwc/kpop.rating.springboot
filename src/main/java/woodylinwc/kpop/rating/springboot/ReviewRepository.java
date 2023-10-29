d0be2b8c4bdb8af3b6f13cc0b66e5d4750586004
Woody Lin <wancheng.lin001@umb.edu>
2023-10-28 22:13:58 -0400

    Create ReviewRepository.java

---
+++ src/main/java/woodylinwc/kpop/rating/springboot/ReviewRepository.java
@@ -0,0 +1,10 @@
+package woodylinwc.kpop.rating.springboot;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends MongoRepository<Review, ObjectId> {
+    
+}
