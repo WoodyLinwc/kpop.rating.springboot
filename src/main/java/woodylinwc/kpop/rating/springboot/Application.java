23036efafbc560d8ebdacbf5246cf259d25e6e48
Woody Lin <wancheng.lin001@umb.edu>
2023-12-04 00:31:40 -0500

    Update Application.java

--- src/main/java/woodylinwc/kpop/rating/springboot/Application.java
+++ src/main/java/woodylinwc/kpop/rating/springboot/Application.java
@@ -2,8 +2,11 @@ package woodylinwc.kpop.rating.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+// @RestController
 public class Application {
 
 	public static void main(String[] args) {
