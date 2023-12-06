5e14ac8eef552948876900b8275bcc00f8d86756
Woody Lin <wancheng.lin001@umb.edu>
2023-12-06 17:02:56 -0500

    Update KpopControlloer.java

--- src/main/java/woodylinwc/kpop/rating/springboot/KpopControlloer.java
+++ src/main/java/woodylinwc/kpop/rating/springboot/KpopControlloer.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/kpop")
+@CrossOrigin(origins = "*")
 public class KpopControlloer {
     
     @Autowired
