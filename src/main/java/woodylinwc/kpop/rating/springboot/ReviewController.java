46f370be8b3ee8233c7c41a979ca622037356374
Woody Lin <wancheng.lin001@umb.edu>
2023-12-06 23:20:30 -0500

    Update ReviewController.java

--- src/main/java/woodylinwc/kpop/rating/springboot/ReviewController.java
+++ src/main/java/woodylinwc/kpop/rating/springboot/ReviewController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewController {
 
     @Autowired
